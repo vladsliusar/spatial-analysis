1a72036bddd45fde1d971e56d0f3ace6d2f362fb
Vlad Sliusar <31670686+vladsliusar@users.noreply.github.com>
2021-01-10 23:17:23 -0800

    remove tilesize & retina detection

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@
 
 var darkscale   = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    tileSize: 512,
     minZoom: 5,
     id: 'mapbox/dark-v10',
-    detectRetina: true,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NDg1bDA1cjYzM280NHJ5NzlvNDMifQ.d6e-nNyBDtmQCVwVNivz7A'
     })
 
